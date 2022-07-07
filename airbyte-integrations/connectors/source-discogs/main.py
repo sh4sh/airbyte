c0c31dfbe37e23db5cb663892adb02e5c4eacb8e
Sunny Hashmi <sunny.hashmi@clouddx.com>
2022-07-07 16:49:11 -0400

    Source Discogs: init connector

---
+++ airbyte-integrations/connectors/source-discogs/main.py
@@ -0,0 +1,12 @@
+# Copyright (c) 2022 Airbyte, Inc., all rights reserved.
+#
+
+
+import sys
+
+from airbyte_cdk.entrypoint import launch
+from source_discogs import SourceDiscogs
+
+if __name__ == "__main__":
+    source = SourceDiscogs()
+    launch(source, sys.argv[1:])
