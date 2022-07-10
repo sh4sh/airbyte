6591b29299148152123baf7ebdb273becda1e9ce
Sunny Hashmi <sunny.hashmi@clouddx.com>
2022-07-10 09:55:09 -0400

    source discogs: start on unit tests

--- airbyte-integrations/connectors/source-discogs/unit_tests/test_source.py
+++ airbyte-integrations/connectors/source-discogs/unit_tests/test_source.py
@@ -4,19 +4,24 @@
 
 from unittest.mock import MagicMock
 
+from pytest import fixture
 from source_discogs.source import SourceDiscogs
 
+@fixture
+def config():
+    return {}
 
-def test_check_connection(mocker):
+def test_check_connection(mocker, requests_mock, config):
     source = SourceDiscogs()
-    logger_mock, config_mock = MagicMock(), MagicMock()
-    assert source.check_connection(logger_mock, config_mock) == (True, None)
+    logger_mock =  MagicMock()
+    config = {"release_id":"249504"} 
+    requests_mock.get("https://dummy")
+    assert source.check_connection(logger_mock, config) == (True, None)
 
 
-def test_streams(mocker):
-    source = SourceDiscogs()
-    config_mock = MagicMock()
-    streams = source.streams(config_mock)
-    # TODO: replace this with your streams number
-    expected_streams_number = 2
-    assert len(streams) == expected_streams_number
+#def test_streams(mocker):
+#    source = SourceDiscogs()
+#    config_mock = MagicMock()
+#    streams = source.streams(config_mock)
+#    expected_streams_number = 1
+#    assert len(streams) == expected_streams_number
--- airbyte-integrations/connectors/source-discogs/unit_tests/test_streams.py
+++ airbyte-integrations/connectors/source-discogs/unit_tests/test_streams.py
@@ -1,33 +1,40 @@
-#
+#/
 # Copyright (c) 2022 Airbyte, Inc., all rights reserved.
 #
 
 from http import HTTPStatus
 from unittest.mock import MagicMock
 
+import requests
+import requests_mock
+import fixture
 import pytest
-from source_discogs.source import DiscogsStream
+from source_discogs.source import Release
 
+@pytest.fixture(name="config")
+def config_fixture():
+    config = {"release_id":"249504"}
+    return config
 
 @pytest.fixture
 def patch_base_class(mocker):
     # Mock abstract methods to enable instantiating abstract class
-    mocker.patch.object(DiscogsStream, "path", "v0/example_endpoint")
-    mocker.patch.object(DiscogsStream, "primary_key", "test_primary_key")
-    mocker.patch.object(DiscogsStream, "__abstractmethods__", set())
+    mocker.patch.object(Release, "path", "v0/example_endpoint")
+    mocker.patch.object(Release, "primary_key", "test_primary_key")
+    mocker.patch.object(Release, "__abstractmethods__", set())
 
 
 def test_request_params(patch_base_class):
-    stream = DiscogsStream()
+    stream = Release(MagicMock())
     # TODO: replace this with your input parameters
-    inputs = {"stream_slice": None, "stream_state": None, "next_page_token": None}
+    inputs = {"stream_slice": None, "stream_state": MagicMock(), "next_page_token": None}
     # TODO: replace this with your expected request parameters
     expected_params = {}
     assert stream.request_params(**inputs) == expected_params
 
 
 def test_next_page_token(patch_base_class):
-    stream = DiscogsStream()
+    stream = Release(MagicMock())
     # TODO: replace this with your input parameters
     inputs = {"response": MagicMock()}
     # TODO: replace this with your expected next page token
@@ -36,26 +43,21 @@ def test_next_page_token(patch_base_class):
 
 
 def test_parse_response(patch_base_class):
-    stream = DiscogsStream()
-    # TODO: replace this with your input parameters
-    inputs = {"response": MagicMock()}
-    # TODO: replace this with your expected parced object
-    expected_parsed_object = {}
+    stream = Release(MagicMock())
+    inputs = {"config": MagicMock()}
+    expected_parsed_object = {"id": "249504" }
     assert next(stream.parse_response(**inputs)) == expected_parsed_object
 
 
 def test_request_headers(patch_base_class):
-    stream = DiscogsStream()
-    # TODO: replace this with your input parameters
-    inputs = {"stream_slice": None, "stream_state": None, "next_page_token": None}
-    # TODO: replace this with your expected request headers
-    expected_headers = {}
+    stream = Release(MagicMock())
+    inputs = { "stream_slice": None, "stream_state": None, "next_page_token": None}
+    expected_headers = {"User-Agent": "AirbyteConnector/0.1 +https://airbyte.com"}
     assert stream.request_headers(**inputs) == expected_headers
 
 
 def test_http_method(patch_base_class):
-    stream = DiscogsStream()
-    # TODO: replace this with your expected http request method
+    stream = Release(MagicMock())
     expected_method = "GET"
     assert stream.http_method == expected_method
 
@@ -72,12 +74,12 @@ def test_http_method(patch_base_class):
 def test_should_retry(patch_base_class, http_status, should_retry):
     response_mock = MagicMock()
     response_mock.status_code = http_status
-    stream = DiscogsStream()
+    stream = Release(MagicMock())
     assert stream.should_retry(response_mock) == should_retry
 
 
 def test_backoff_time(patch_base_class):
     response_mock = MagicMock()
-    stream = DiscogsStream()
+    stream = Release(MagicMock())
     expected_backoff_time = None
     assert stream.backoff_time(response_mock) == expected_backoff_time
