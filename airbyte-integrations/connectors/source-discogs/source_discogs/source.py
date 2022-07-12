7eb538e619e19fac0a8bc7cade0e6492f705c296
Sunny Hashmi <6833405+sh4sh@users.noreply.github.com>
2022-07-12 19:31:40 -0400

    source discogs: improve check_connection and fix formatting

--- airbyte-integrations/connectors/source-discogs/source_discogs/source.py
+++ airbyte-integrations/connectors/source-discogs/source_discogs/source.py
@@ -10,30 +10,42 @@ import requests
 from airbyte_cdk.sources import AbstractSource
 from airbyte_cdk.sources.streams import Stream
 from airbyte_cdk.sources.streams.http import HttpStream
+from airbyte_cdk.logger import AirbyteLogger
+from airbyte_cdk.models import SyncMode
+
 from airbyte_cdk.sources.streams.http.auth import NoAuth
 # Don't need authentication yet, but maybe later
 # from airbyte_cdk.sources.streams.http.auth import TokenAuthenticator
 
-# Basic full refresh stream
+# Abstract base class meant to contain all the common functionality at the API level e.g: the API base URL,
+# pagination strategy, parsing responses etc..
+
+
 class DiscogsStream(HttpStream, ABC):
 
     url_base = "https://api.discogs.com/"
 
-    primary_key = None
-
-    def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
+    def next_page_token(
+            self,
+            response: requests.Response
+    ) -> Optional[Mapping[str, Any]]:
         return None
-#        return {'page': response.json()['page'] + 1}
+        # return {'page': response.json()['page'] + 1}
+        # Not all endpoints offer pagination so we should leave this empty for now
 
     def request_params(
-        self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, any] = None, next_page_token: Mapping[str, Any] = None
+            self,
+            stream_state: Mapping[str, Any],
+            stream_slice: Mapping[str, Any] = None,
+            next_page_token: Mapping[str, Any] = None,
     ) -> MutableMapping[str, Any]:
-        # Discogs default pagination is 50, max is 100
-        # Turns out we do not need pagination for the Release endpoint, however is needed in the future
         return {}
 
-    def request_headers(self, **kwargs) -> Mapping[str, Any]:
-        # All requests need a User-Agent or Discogs will return 404
+    # All requests need a User-Agent or Discogs will return 404
+    def request_headers(
+            self,
+            **kwargs
+    ) -> Mapping[str, Any]:
         return {"User-Agent": "AirbyteConnector/0.1 +https://airbyte.com"}
 
     def parse_response(
@@ -42,14 +54,21 @@ class DiscogsStream(HttpStream, ABC):
             **kwargs
     ) -> Iterable[Mapping]:
         #        yield {}
-        return [response.json()]
+        response_json = response.json()
+        yield response_json
 
-
+# The Release resource endpoint will pull all metadata associated with a particular physical or digital object released by one or more Artists.
+# release_id: The Release ID e.g. 249504
+# curr_abbr: Currency for marketplace data. Defaults to the authenticated users currency.
 class Release(DiscogsStream):
 
     primary_key = "id"
 
-    def __init__(self, config: Mapping[str, Any], **kwargs):
+    def __init__(
+            self,
+            config: Mapping[str, Any],
+            **kwargs
+    ):
         super().__init__()
         self.release_id = config['release_id']
         self.curr_abbr = config['curr_abbr']
@@ -77,19 +96,27 @@ class Release(DiscogsStream):
 
 # Source
 class SourceDiscogs(AbstractSource):
-    def check_connection(self, logger, config) -> Tuple[bool, Any]:
+    def check_connection(
+        self,
+        logger: AirbyteLogger,
+        config: Mapping[str, Any]
+    ) -> Tuple[bool, Any]:
         logger.info("Checking Discogs API connection...")
         try:
-            url = "https://api.discogs.com/releases/" + config["release_id"]
-            headers = {
-                # All requests need a User-Agent or Discogs will return 404
-                "User-Agent": "AirbyteConnector/0.1 +https://airbyte.com"}
-            requests.get(url, headers=headers)
+            args = {"authenticator": NoAuth(), "config": config}
+            release_stream = Release(**args)
+            release_items = release_stream.read_records(sync_mode=SyncMode.full_refresh)
+            item = next(release_items)
+            logger.info(f"Successfully connected to Discogs API. Pulled one release with title: {item['title']}")
             return True, None
         except requests.exceptions.RequestException as e:
             return False, e
 
-    def streams(self, config: Mapping[str, Any]) -> List[Stream]:
-        auth = NoAuth()
-#        return [Customers(authenticator=auth), Employees(authenticator=auth)]
-        return [Release(authenticator=auth, config=config)]
+    def streams(
+        self,
+        config: Mapping[str, Any]
+    ) -> List[Stream]:
+        args = {"authenticator": NoAuth(), "config": config}
+        return [
+            Release(**args)
+        ]
